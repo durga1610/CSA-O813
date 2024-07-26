77265a1b241092f3da9262f572ea8ca59a2d632b
durga1610 <durga.kdm16@gmail.com>
2024-07-26 13:35:11 +0530

    Add files via upload

---
+++ gratest in three.py
@@ -0,0 +1,9 @@
+a=int(input("enter the first number="))
+b=int(input("enter the second number="))
+c=int(input("enter the third number="))
+if a>b and a>c:
+    print(a,"a is bigger")
+if b>a and b>c:
+    print(b,"b is bigger")
+else:
+    print(c,"c is bigger")
---
+++ gratest in two.py
@@ -0,0 +1,7 @@
+a=eval(input("enter the value of a"))
+b=eval(input("enter the value of b"))
+if a>b:
+    print(a,"a is bigger")
+else:
+    print(b,"b is bigger")
+
